0365890140c20d6a9c1a96d6930ce3e768133ff3
Franklin Shera <fshera96@gmail.com>
2025-11-17 07:03:57 +0300

    home page redirect

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,11 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import type { RequestHandler } from "@builder.io/qwik-city";
 
-export default component$(() => {
-  return (
-    <>
-      <h1>Hi 👋</h1>
-      <div>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </div>
-    </>
-  );
-});
+export const onGet: RequestHandler = async ({ sharedMap, redirect, url }) => {
+  const user = sharedMap.get("user");
 
-export const head: DocumentHead = {
-  title: "Welcome to Qwik",
-  meta: [
-    {
-      name: "description",
-      content: "Qwik site description",
-    },
-  ],
+  if (!user) {
+    throw redirect(302, new URL("/signin", url).toString());
+  }
+
+  throw redirect(302, new URL("/account", url).toString());
 };
