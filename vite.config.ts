6eda588c72fb2f0c4e07e4f802d9819c7c5edbc5
Franklin Shera <fshera96@gmail.com>
2025-11-18 06:24:10 +0300

    reset vite config

--- vite.config.ts
+++ vite.config.ts
@@ -22,11 +22,7 @@ errorOnDuplicatesPkgDeps(devDependencies, dependencies);
  */
 export default defineConfig((): UserConfig => {
   return {
-    plugins: [
-      qwikCity({ trailingSlash: false }),
-      qwikVite(),
-      tsconfigPaths({ root: "." }),
-    ],
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths({ root: "." })],
     // This tells Vite which dependencies to pre-build in dev mode.
     optimizeDeps: {
       // Put problematic deps that break bundling here, mostly those with binaries.
